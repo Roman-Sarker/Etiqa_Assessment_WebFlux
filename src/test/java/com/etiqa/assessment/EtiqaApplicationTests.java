459ebc539279270921ce55c92fb39884b5ada50f
Roman Sarker <mdroman601@gmail.com>
2024-09-13 12:55:52 +0600

    First Commit - 13/09/2024

---
+++ src/test/java/com/etiqa/assessment/EtiqaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etiqa.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtiqaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
